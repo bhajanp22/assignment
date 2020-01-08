b356436a28fba0ec742e16620bf0a5dd5a215fd7
Bhajan <bpoonia@Bhajans-MacBook-Air.local>
2020-01-08 22:34:35 +0530

    remove files

--- src/main/java/com/fk/page/cart/FKChkoutPageConstants.java
+++
@@ -1,9 +0,0 @@
-package com.fk.page.cart;
-
-public interface FKChkoutPageConstants {
-
-	String FKURL = "https://www.flipkart.com/";
-	public static final String USERNAME = "8949945432";
-	public static final String PASSWORD = "test@123";
-
-}
